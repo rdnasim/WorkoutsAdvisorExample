5d9a66f5081236fa925313cca88a45c3fba5d234
rdnasim <rdislamnasim78@gmail.com>
2017-05-11 14:28:42 +0600

    Updated

--- app/src/main/java/com/example/rdnasim/workoutsadvisorexample/MainActivity.java
+++ app/src/main/java/com/example/rdnasim/workoutsadvisorexample/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.rdnasim.workoutsadvisorexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnClickFindWorkout(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        Spinner spinner = (Spinner) findViewById(R.id.workoutType);
+
+        String string = String.valueOf(spinner.getSelectedItem());
+        textView.setText(string);
     }
 }
